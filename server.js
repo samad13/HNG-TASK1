af240c9d8982abe63491efab69539774f86fc6f1
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2022-11-04 10:00:28 +0100

    adding hng stage two task

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
 const  express =  require("express");
 const app = express();
 
-//app.use(express.urlencoded({extended: true}));
+const PORT = process.env.PORT || 5000
 
-app.get('/',function(req, res){
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+
+app.get('/',(req, res)=>{
   res.json(
-    {slackUsername:"Hello World",backend: true,age: 29, bio:"born in kwara and last born of a family of three"} );
+    {slackUsername:"S13",backend: true,age: 29, bio:"born in kwara and last born of a family of three"} );
 });
 
 
 
-app.listen(5000,function(){
-    console.log("starting server")  
+
+app.get('/hng2',(req, res)=>{
+  res.json("this is for task2");
+});
+
+const operationTypes = ['addition', 'subtraction', 'multiplication'];
+
+app.post('/hng2', (req, res) => {
+    let x = parseFloat(req.body.x);
+    let y = parseFloat(req.body.y);
+    let operation = req.body.operation_type.toLowerCase();
+    let slackUsername = "S13";
+    if(isNaN(x) || isNaN(y)){
+        res.status(400).send("input field not a number");
+    } else if(!operationTypes.includes(operation)){
+        res.status(400).send("not valid operation types");
+    } else {
+        let result;
+        
+        switch(operation){
+            case "addition":
+                result = x + y;
+                break;
+            case "subtraction":
+                result = x - y;
+                break;
+            case "multiplication":
+                result = x * y;
+                break;
+            default:
+                result = null; 
+        }
+        const Response = {
+            "slackUsername":slackUsername,
+            "result": result,
+            "operation_type": req.body.operation_type
+        }
+        res.status(200).json(Response);
+    }
+});
+
+
+
+app.listen(PORT,() =>{
+    console.log(`starting server ${PORT}`)  
 })
+
