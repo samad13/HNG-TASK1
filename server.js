debc8c9a0f709eff479102cc8a4b0cfc298260fe
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2023-11-17 18:11:46 +0100

    contingency

--- server.js
+++ server.js
@@ -1,63 +1,106 @@
-const  express =  require("express");
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/',(req, res)=>{
-  res.json(
-    {slackUsername:"S13",backend: true,age: 29, bio:"born in kwara and last born of a family of three"} );
+
+app.get('/split-payments/compute', (req, res) => {
+    res.json("this is for task2");
 });
 
+app.post('/split-payments/compute', (req, res) => {
+    try {
 
 
+        const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
 
-app.get('/hng2',(req, res)=>{
-  res.json("this is for task2");
-});
-
-const operationTypes = ['addition', 'subtraction', 'multiplication'];
-
-app.post('/hng2', (req, res) => {
-    let x = parseFloat(req.body.x);
-    let y = parseFloat(req.body.y);
-    let operation = req.body.operation_type.toLowerCase();
-    let slackUsername = "S13";
-    if(isNaN(x) || isNaN(y)){
-        res.status(400).send("input field not a number");
-    } else if(!operationTypes.includes(operation)){
-        res.status(400).send("not valid operation types");
-    } else {
-        let result;
-        
-        switch(operation){
-            case "addition":
-                result = x + y;
-                break;
-            case "subtraction":
-                result = x - y;
-                break;
-            case "multiplication":
-                result = x * y;
-                break;
-            default:
-                result = null; 
+        if (SplitInfo.length < 1 || SplitInfo.length > 20) {
+            return res.status(400).json({ error: 'SplitInfo must contain between 1 and 20 entities.' });
         }
-        const Response = {
-            "slackUsername":slackUsername,
-            "result": result,
-            "operation_type": req.body.operation_type
+
+        SplitInfo.sort((a, b) => {
+            const order = { FLAT: 1, PERCENTAGE: 2, RATIO: 3 };
+            return order[a.SplitType] - order[b.SplitType];
+        });
+
+        let balance = Amount;
+
+        let totalSplitAmount = 0;
+
+
+        const splitBreakdown = [];
+
+        for (const splitEntity of SplitInfo) {
+
+            let splitAmount;
+            if (splitEntity.SplitType === 'FLAT') {
+                splitAmount = splitEntity.SplitValue;
+                balance - splitAmount
+            } else if (splitEntity.SplitType === 'PERCENTAGE') {
+
+                splitAmount = (splitEntity.SplitValue / 100) * balance;
+                balance - splitAmount
+
+            } else if (splitEntity.SplitType === 'RATIO') {
+                const totalRatio = SplitInfo
+                    .filter(entity => entity.SplitType === 'RATIO')
+                    .reduce((total, entity) => total + entity.SplitValue, 0);
+
+                splitAmount = (splitEntity.SplitValue / totalRatio) * balance;
+                balance - splitAmount
+
+
+
+            }
+            balance - splitAmount;
+
+            totalSplitAmount += splitAmount;
+
+
+            if (totalSplitAmount > Amount) {
+                return res.status(400).json({ error: 'Sum of split amounts cannot be greater than the transaction amount.' });
+            }
+
+            if (splitAmount > balance) {
+                return res.status(400).json({ error: 'Split amount cannot be greater than the remaining balance.' });
+            }
+
+            if (splitAmount < 0) {
+                return res.status(400).json({ error: 'Split amount cannot be less than 0.' });
+            }
+            balance = Math.max(balance - splitAmount, 0);
+            if (balance < 0) {
+                return res.status(400).json({ error: 'Final balance cannot be less than 0.' });
+            }
+
+            splitBreakdown.push({
+                SplitEntityId: splitEntity.SplitEntityId,
+                Amount: splitAmount,
+            });
         }
-        res.status(200).json(Response);
+
+
+        const response = {
+            ID,
+            Balance: balance,
+            SplitBreakdown: splitBreakdown,
+        };
+
+        res.status(200).json(response);
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 
 
-app.listen(PORT,() =>{
-    console.log(`starting server ${PORT}`)  
-})
 
+app.listen(PORT, () => {
+    console.log(`starting server ${PORT}`)
+})
